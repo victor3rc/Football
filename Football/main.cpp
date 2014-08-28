cd4fa2b98836a43e4f9f4b4ff7c8008f0ed0e180
Victor Cardozo <victor3rc@gmail.com>
2014-08-28 10:46:03 +0100

    Initial Commit

---
+++ Football/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Football
+//
+//  Created by Victor Cardozo on 28/08/2014.
+//  Copyright (c) 2014 Victor Cardozo. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
